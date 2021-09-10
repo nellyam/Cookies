9e42e63552252f564ec993684da330638fee5051
Nelly <nellynellyam@gmail.com>
2021-09-10 11:46:22 +0200

    some new changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const today = new Date();
-const nextWeek = new Date(today.getTime() +  7 * 24 * 60 * 60 * 1000);
+
+const nextWeek = new Date(today.getTime() +  7 * 24 * 60 * 60 * 1000); 
+
 let day = ('0' + nextWeek).slice(9, 11);
 let month = ('0' +(today.getMonth() + 1)).slice(-2);
 let year = today.getFullYear();
-
 document.querySelector("input[type='date']").value = `${year}-${month}-${day}`;
-console.log(day);
+
